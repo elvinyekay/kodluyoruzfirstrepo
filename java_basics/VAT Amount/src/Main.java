2292f5f85652a8beaa15d0bc2d452942542da348
ElvinYekayev <elvinyekay@gmail.com>
2025-05-18 17:16:21 +0400

    VAT amount

--- java_basics/VAT Amount/src/Main.java
+++ java_basics/VAT Amount/src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
-
         Scanner scanner = new Scanner(System.in);
 
         double amount, vatAmount, totalAmount;
         int VATPercentage = 0;
+
         System.out.println("Enter the fee amount:");
         amount = scanner.nextDouble();
+
         if (amount > 0 && amount<= 1000){
             VATPercentage = 8;
         }if (amount > 1000){
