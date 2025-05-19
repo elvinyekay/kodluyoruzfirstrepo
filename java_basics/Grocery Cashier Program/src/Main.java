8097dcf88484fbdbe9409da1008471b18746452f
ElvinYekayev <elvinyekay@gmail.com>
2025-05-19 05:41:19 +0400

    Grocery Cashier Program

---
+++ java_basics/Grocery Cashier Program/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+         *
+         * Meyveler ve KG Fiyatları
+         *
+         * Armut : 2,14 TL
+         * Elma : 3,67 TL
+         * Domates : 1,11 TL
+         * Muz: 0,95 TL
+         * Patlıcan : 5,00 TL
+         */
+        Scanner scanner = new Scanner(System.in);
+        final double pearPrice = 2.14;
+        final double applePrice = 3.67;
+        final double tomatoPrice = 1.11;
+        final double bananaPrice = 0.95;
+        final double eggplantPrice = 5.0;
+        double pearKg, appleKg, tomatoKg, bananaKg, eggplantKg,totalAmount;
+
+        System.out.println("Pear kg: ");
+        pearKg = scanner.nextInt();
+        System.out.println("Apple kg: ");
+        appleKg = scanner.nextInt();
+        System.out.println("Tomato kg: ");
+        tomatoKg = scanner.nextInt();
+        System.out.println("Banana kg: ");
+        bananaKg = scanner.nextInt();
+        System.out.println("Eggplant kg: ");
+        eggplantKg = scanner.nextInt();
+
+        totalAmount =
+                (pearKg * pearPrice) + (appleKg * applePrice) + (tomatoKg * tomatoPrice) + (bananaKg * bananaPrice) + (eggplantKg * eggplantPrice);
+
+        System.out.println("Total amount:" + totalAmount);
+    }
+}
