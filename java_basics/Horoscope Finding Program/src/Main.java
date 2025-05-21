c1a3dede17eadbcb081705a781a9a3c704d375c6
ElvinYekayev <elvinyekay@gmail.com>
2025-05-21 06:57:33 +0400

    Horoscope Finding Program

--- java_basics/Horoscope Finding Program/src/Main.java
+++ java_basics/Horoscope Finding Program/src/Main.java
@@ -12,77 +12,127 @@ public class Main {
         day = input.nextInt();
 
         if (month == 1) {
-            if (day < 22) {
-                System.out.println("Capricorn");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Capricorn");
+                } else {
+                    System.out.println("Aquarius");
+                }
             } else {
-                System.out.println("Aquarius");
+                System.out.println("Invalid day");
             }
         } else if (month == 2) {
-            if (day < 22) {
-                System.out.println("Aquarius");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Aquarius");
+                } else {
+                    System.out.println("Pisces");
+                }
             } else {
-                System.out.println("Pisces");
+                System.out.println("Invalid day");
             }
         } else if (month == 3) {
-            if (day < 22) {
-                System.out.println("Pisces");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Pisces");
+                } else {
+                    System.out.println("Aries");
+                }
             } else {
-                System.out.println("Aries");
+                System.out.println("Invalid day");
             }
         } else if (month == 4) {
-            if (day < 22) {
-                System.out.println("Aries");
-            } else {
-                System.out.println("Taurus");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Aries");
+                } else {
+                    System.out.println("Taurus");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
         } else if (month == 5) {
-            if (day < 22) {
-                System.out.println("Taurus");
-            } else {
-                System.out.println("Gemini");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Taurus");
+                } else {
+                    System.out.println("Gemini");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
         } else if (month == 6) {
-            if (day < 22) {
-                System.out.println("Gemini");
-            } else {
-                System.out.println("Cancer");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Gemini");
+                } else {
+                    System.out.println("Cancer");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
         } else if (month == 7) {
-            if (day < 22) {
-                System.out.println("Cancer");
-            } else {
-                System.out.println("Leo");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Cancer");
+                } else {
+                    System.out.println("Leo");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
         } else if (month == 8) {
-            if (day < 22) {
-                System.out.println("Leo");
-            } else {
-                System.out.println("Virgo");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Leo");
+                } else {
+                    System.out.println("Virgo");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
         } else if (month == 9) {
-            if (day < 22) {
-                System.out.println("Virgo");
-            } else {
-                System.out.println("Libra");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Virgo");
+                } else {
+                    System.out.println("Libra");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
         } else if (month == 10) {
-            if (day < 22) {
-                System.out.println("Libra");
-            } else {
-                System.out.println("Scorpio");
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Libra");
+                } else {
+                    System.out.println("Scorpio");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
-        }else if (month == 11) {
-            if (day < 22) {
-                System.out.println("Scorpio");
-            } else {
-                System.out.println("Sagittarius");
+        } else if (month == 11) {
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Scorpio");
+                } else {
+                    System.out.println("Sagittarius");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
-        }else if (month == 12) {
-            if (day < 22) {
-                System.out.println("Sagittarius");
-            } else {
-                System.out.println("Capricorn");
+        } else if (month == 12) {
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Sagittarius");
+                } else {
+                    System.out.println("Capricorn");
+                }
+            }else {
+                System.out.println("Invalid day");
             }
+        }else{
+            System.out.println("Invalid month");
         }
     }
 }
