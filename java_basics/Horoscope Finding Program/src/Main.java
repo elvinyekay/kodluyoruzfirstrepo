f152d39e33bdcd130d8b25332e50b652b4a3a05a
ElvinYekayev <elvinyekay@gmail.com>
2025-05-21 06:51:24 +0400

    Horoscope Finding Program

---
+++ java_basics/Horoscope Finding Program/src/Main.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int month, day;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter month number: ");
+        month = input.nextInt();
+
+        System.out.println("Enter day number: ");
+        day = input.nextInt();
+
+        if (month == 1) {
+            if (day < 22) {
+                System.out.println("Capricorn");
+            } else {
+                System.out.println("Aquarius");
+            }
+        } else if (month == 2) {
+            if (day < 22) {
+                System.out.println("Aquarius");
+            } else {
+                System.out.println("Pisces");
+            }
+        } else if (month == 3) {
+            if (day < 22) {
+                System.out.println("Pisces");
+            } else {
+                System.out.println("Aries");
+            }
+        } else if (month == 4) {
+            if (day < 22) {
+                System.out.println("Aries");
+            } else {
+                System.out.println("Taurus");
+            }
+        } else if (month == 5) {
+            if (day < 22) {
+                System.out.println("Taurus");
+            } else {
+                System.out.println("Gemini");
+            }
+        } else if (month == 6) {
+            if (day < 22) {
+                System.out.println("Gemini");
+            } else {
+                System.out.println("Cancer");
+            }
+        } else if (month == 7) {
+            if (day < 22) {
+                System.out.println("Cancer");
+            } else {
+                System.out.println("Leo");
+            }
+        } else if (month == 8) {
+            if (day < 22) {
+                System.out.println("Leo");
+            } else {
+                System.out.println("Virgo");
+            }
+        } else if (month == 9) {
+            if (day < 22) {
+                System.out.println("Virgo");
+            } else {
+                System.out.println("Libra");
+            }
+        } else if (month == 10) {
+            if (day < 22) {
+                System.out.println("Libra");
+            } else {
+                System.out.println("Scorpio");
+            }
+        }else if (month == 11) {
+            if (day < 22) {
+                System.out.println("Scorpio");
+            } else {
+                System.out.println("Sagittarius");
+            }
+        }else if (month == 12) {
+            if (day < 22) {
+                System.out.println("Sagittarius");
+            } else {
+                System.out.println("Capricorn");
+            }
+        }
+    }
+}
