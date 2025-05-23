08e2a6ae70d4a7a16774855e22f2ac048807c09e
ElvinYekayev <elvinyekay@gmail.com>
2025-05-23 22:40:14 +0400

    Reverse Triangle Pattern

--- java_basics/Reverse Triangle Pattern/src/Main.java
+++ java_basics/Reverse Triangle Pattern/src/Main.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a row number: ");
+        System.out.println("How many rows should the triangle have?: ");
         int row = sc.nextInt();
+
+        if (row <=0){
+            System.out.println("Row should be greater than 0");
+            return;
+        }
 
         for (int i = row; i > 0; i--) {
             for (int j = 0; j < (i * 2) - 1; j++) {
