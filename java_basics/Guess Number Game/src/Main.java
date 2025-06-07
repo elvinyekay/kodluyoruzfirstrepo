efad7d30a93d4770bdc36e3819fe902ba2faa1a9
ElvinYekayev <elvinyekay@gmail.com>
2025-06-07 14:51:58 +0400

    Guess Number Game

--- java_basics/Guess Number Game/src/Main.java
+++ java_basics/Guess Number Game/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,40 +6,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
-        int right = 0;
+        int right = 0, input;
         boolean isWin = false;
         int[] wrong = new int[5];
+        boolean mistake = false;
 
         while (right < 5) {
             int random = rand.nextInt(100);
             System.out.println(random);
             System.out.println("Enter your guess number: (0 - 100)");
-            int input = scanner.nextInt();
+            input = scanner.nextInt();
 
             if (input < 0 || input > 90) {
                 System.out.println("Please enter number between 0 - 100");
+                if (mistake){
+                    System.out.println("Too many wrong inputs, your right: " + (5- (++right)));
+                }else{
+                    mistake = true;
+                    System.out.println("Writing wrong number is take 1 life");
+                }
                 continue;
             }
 
             if (input == random) {
-                if(isWin){
-
-                }
                 System.out.println("Your number: " + input);
                 System.out.println("Guess number: " + random);
                 System.out.println("Congratulations. You guess the number.");
                 isWin = true;
                 break;
             } else {
-                wrong[right++] = input;
-
                 System.out.println("Your number: " + input);
                 System.out.println("Guess number: " + random);
                 System.out.println("Sorry, not a right number, try again.");
                 if (input > random){
                     System.out.println("The select Number greater than guess number.");
+                }else{
+                    System.out.println("The select Number smaller than guess number.");
                 }
             }
+            wrong[right++] = input;
             System.out.println((5 - right) + " right.");
         }
         if (!isWin){
